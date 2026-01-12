c84c383e06128c3760cbe42e923cc9376ddb629c
Rao Huzaifa <138229711+RaoHuzaifa232@users.noreply.github.com>
2026-01-12 23:48:26 +0500

    route fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { Header } from './components/header';
 import Events from './pages/Events/Events';
 import Home from './pages/Home/home';
-
+import Hooks from './pages/Hooks/Hooks';
 function App() {
   return (
     // Jsx syntax
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Events />} />
+          <Route path="/hooks" element={<Hooks />} />
         </Routes>
       </BrowserRouter>
     </>
